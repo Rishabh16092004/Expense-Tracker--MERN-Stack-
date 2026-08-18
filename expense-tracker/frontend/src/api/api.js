1306f679dee27b00eaa60c85eaa2d9e3e0661ff5
Rishabh16092004 <rishabhjaiswal16092004@gmail.com>
2026-08-18 18:40:11 +0530

    update frontend api.js

--- expense-tracker/frontend/src/api/api.js
+++ expense-tracker/frontend/src/api/api.js
@@ -1,10 +1,18 @@
 import axios from "axios";
 
-const API = axios.create({ baseURL: "http://localhost:5000/api" });
+const API_URL = import.meta.env.VITE_API_URL;
+
+const API = axios.create({
+  baseURL: API_URL
+});
 
 API.interceptors.request.use((req) => {
   const token = localStorage.getItem("token");
-  if (token) req.headers.Authorization = `Bearer ${token}`;
+
+  if (token) {
+    req.headers.Authorization = `Bearer ${token}`;
+  }
+
   return req;
 });
 
